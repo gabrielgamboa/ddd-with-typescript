15c98b68deaa8a44fdd73f9bd8b588badfb21af2
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-10-20 11:03:58 -0300

    feat: base ddd project create

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,15 @@
+export class Slug {
+    constructor(
+        public value: string,
+    ) {}
+
+
+    /**
+     * Receives a string and normalize it as a slug.
+     * Example: "An example title" => "an-example-title".
+     * @param text {string} The string to normalize
+     */
+    static createFromText(text: string) {
+
+    }
+}
---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswersRepository: AnswersRepository = {
+    create: async (answer: Answer) => {
+        return;
+    }
+}
+
+test('Create an answer', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+    const answer = await answerQuestion.execute({
+        content: 'New Answer',
+        questionId: '1',
+        instructorId: '1'
+    });
+
+    expect(answer).toBeTruthy()
+    expect(answer.content).toEqual('New Answer')
+})
